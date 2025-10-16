8bdac37a7c7c94d4ec2744ec1c9d61c991e00055
bellanzo <abimbolaomisakin678@gmail.com>
2025-10-16 01:52:35 +0100

    fixed some issues

--- src/services/cat.service.ts
+++ src/services/cat.service.ts
@@ -4,6 +4,7 @@ import { config } from "@/config/app.config";
 export const fetchedCatFact = async (): Promise<string> => {
   try {
     const response = await axios.get(config.catFactApi, { timeout: 5000 });
+    console.log("Successfully fetched cat fact");
     return response.data.fact;
   } catch (error: any) {
     console.log("Error fetching cat fact:", error.message);
